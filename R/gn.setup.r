8c1ac5ee2bd798a9a4c7a5545b859e9fbfe1b566
remymoine <remymoine95@gmail.com>
2019-02-11 14:54:26 +0100

    Deux modifications de la fonction gn.setup: Intégration du package DBI ligne 57 sinon ne tourne pas et écriture de la sortie ligne 59.
    Ajout d'un exemple dans la fonction section @usage et précision des différents paramètres @param.

--- R/gn.setup.r
+++ R/gn.setup.r
@@ -3,13 +3,14 @@
 #' Cette fonction permet de configurer la connexion à la base de données de votre instance GeoNature. Elle crée un objet nécessaire au bon fonctionnement de nombreuses autres fonctions du package, et doit être jouée au début de votre session de travail.
 #' Si vous supprimez cet objet setupDB, veuillez rejouer la fonction gn.setup() pour reconfigurer la connexion à votre base.
 #'
-#' @param dbname nom de la base de données
-#' @param host nom de l'hôte de votre base de données
-#' @param port port de connexion à votre base de données 
-#' @param user nom de l'utilisateur pour se connecter à votre base de données.
+#' @param dbname nom de la base de données (type "character")
+#' @param host nom de l'hôte de votre base de données (type "character")
+#' @param port port de connexion à votre base de données (type integer)
+#' @param user nom de l'utilisateur pour se connecter à votre base de données (type "character")
 #'
 #' @return  un object setupDB permettant aux autres fonctions du package d'établir une connexion à votre base de données GeoNature
 #'
+#' @usage setupDB<-gn.setup()
 #'
 #' @author Donovan Maillard <donovan.maillard@gmail.com>
 #' @export
@@ -17,16 +18,16 @@
 #'
 
 
-gn.setup <- function (dbname,host,port,user) 
+gn.setup <- function (dbname,host,port,user)
 	{
-		if(hasArg(dbname)==FALSE) 
+		if(hasArg(dbname)==FALSE)
 			{
 			print("Veuillez renseigner le nom de votre base de données GeoNature :")
 			dbname<-scan (what="character",nmax=1, quiet=T)
 			}
 		else {}
-	
-		if(hasArg(host)==FALSE) 
+
+		if(hasArg(host)==FALSE)
 			{
 			print("Veuillez renseigner l'hôte de votre base de données GeoNature :")
 			host<-scan (what="character",nmax=1, quiet=T)
@@ -52,9 +53,11 @@ gn.setup <- function (dbname,host,port,user)
 
 	print("Patientez quelques secondes, nous testons la connexion...")
 
-	connect_test <- try(dbConnect(dbDriver("PostgreSQL"), dbname = dbname,host = host, port = port,user = user, password = pass), silent=T)
+	connect_test <- try(DBI::dbConnect(dbDriver("PostgreSQL"), dbname = dbname,host = host, port = port,user = user, password = pass), silent=T)
  		if(is(connect_test,"try-error")) {print("La connexion à votre base de données GeoNature a échoué")}
-  		else {setupDB <<- dbConnect(dbDriver("PostgreSQL"), dbname = dbname,host = host, port = port,user = user, password = pass)
+  		else {setupDB <- dbConnect(dbDriver("PostgreSQL"), dbname = dbname,host = host, port = port,user = user, password = pass)
   			print("La connexion à PostgreSQL a bien été configurée. Un objet setupDB a été créé, il sera nécessaire pour les autres fonctions de ce package. Si vous le supprimez, veuillez relancer la fonction gn_setup")
-			}
+  			setupDB}
+
 }
+
